a2059e75062a4df34828dd15edac9aaf791f2a0a
Faizan <faizanshk290@gmail.com>
2025-02-15 15:59:44 +0530

    Added my first test cases

---
+++ cypress/integration/sample.js
@@ -0,0 +1,7 @@
+describe("My First Test", () => {
+    it("My first case", () => {
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+        cy.get('#dropdown-class-example').select('option2').should('have.value', 'option2');
+        cy.get('#checkBoxOption2').check().should('be.checked');
+    });
+})
