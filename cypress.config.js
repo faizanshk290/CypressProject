93878330fed59ee50a913e0ff3b5dd1f4f8f1d51
Faizan <faizanshk290@gmail.com>
2025-02-15 16:10:07 +0530

    Added changes in sample.js file

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: false,
+    json: true,
+    embeddedScreenshots: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     specPattern: "cypress/integration/*.js",
   },
