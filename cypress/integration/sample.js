d9b3291945a8d1b82fd989dd8de207d1a0ef40da
Faizan <faizanshk290@gmail.com>
2025-05-01 11:40:27 +0530

    Added new steps in sample.js file

--- cypress/integration/sample.js
+++ cypress/integration/sample.js
@@ -3,5 +3,6 @@ describe("My First Test", () => {
         cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
         cy.get('#dropdown-class-example').select('option2').should('have.value', 'option2');
         cy.get('#checkBoxOption2').check().should('be.checked');
+        cy.get('#checkBoxOption2').uncheck().should('not.be.checked');
     });
 })
